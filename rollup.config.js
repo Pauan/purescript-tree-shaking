2a98e0c8a3b90e41c46f232f89e3be0c44ccdb0c
Pauan <pcxunlimited@gmail.com>
2016-12-27 08:40:09 -1000

    Updating to a new version of rollup-plugin-purs

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,17 @@
 import resolve from "rollup-plugin-node-resolve";
 //import purs from "./rollup-plugin-purs.js";
 import purs from "rollup-plugin-purs";
+//import sourcemaps from "rollup-plugin-sourcemaps";
 //import commonjs from "rollup-plugin-commonjs";
 
 export default {
-  entry: "Main.js",
+  entry: "src/Main.purs",
   dest: "dist/rollup.js",
   format: "iife",
+  sourceMap: true,
   plugins: [
-    resolve(),
-    /*commonjs({
-      exclude: "node_modules/**",
-      namedExports: {
-        "./output/Main/index.js": ["main"]
-      }
-    })*/
-    purs({
-      exclude: "node_modules/**"
-    })
+    //sourcemaps(),
+    purs(),
+    resolve()
   ]
 };
